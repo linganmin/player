96f140cc1f5a9bea621092e114e6e688a1545f42
linganmin <saboran@163.com>
2020-07-05 08:51:20 +0800

    play

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,58 +1,143 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="index">
+    <el-card class="box-card">
+      <div class="parse">
+        <el-select v-model="parse_url"
+                   clearable
+                   placeholder="请选择解析地址"
+                   class="el-select-custom">
+          <el-option v-for="item in apis"
+                     :key="item.value"
+                     :label="item.label"
+                     :value="item.value">
+          </el-option>
+        </el-select>
+        <el-input class="el-input-custom"
+                  v-model="play_url"
+                  placeholder="请输入视频地址"></el-input>
+        <el-button class="el-button-custom"
+                   type="primary"
+                   @click="play"
+                   round>开始解析</el-button>
+
+        <hr>
+
+        <div class="sites">
+          <el-button type="success">爱奇艺</el-button>
+          <el-button type="info">芒果TV</el-button>
+          <el-button type="warning">腾讯视频</el-button>
+          <el-button type=danger>优酷土豆</el-button>
+
+        </div>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "Index",
+  data() {
+    return {
+      parse_url: "",
+      play_url: "",
+      apis: [
+        {
+          value: "http://api.baiyug.vip/index.php?url=",
+          lable: "http://api.baiyug.vip/index.php?url="
+        },
+        {
+          value: "http://www.82190555.com/index/qqvod.php?url=",
+          lable: "http://www.82190555.com/index/qqvod.php?url="
+        },
+        {
+          value: "http://jiexi.071811.cc/jx2.php?url=",
+          lable: "http://jiexi.071811.cc/jx2.php?url="
+        },
+        {
+          value: "http://beaacc.com/api.php?url=",
+          lable: "http://beaacc.com/api.php?url="
+        },
+        {
+          value: "http://qxkkk.bid/jx/jx.php?url=",
+          lable: "http://qxkkk.bid/jx/jx.php?url="
+        },
+        {
+          value: "http://o8ve.cn/1/?url=",
+          lable: "http://o8ve.cn/1/?url="
+        },
+        {
+          value: "http://api.xyingyu.com/?url=",
+          lable: "http://api.xyingyu.com/?url="
+        },
+        {
+          value: "http://api.greatchina56.com/?url=",
+          lable: "http://api.greatchina56.com/?url="
+        },
+        {
+          value: "http://jx.618g.com/?url=",
+          lable: "http://jx.618g.com/?url="
+        },
+        {
+          value: "http://jx.iaeec.cn/jx.php/?url=",
+          label: "http://jx.iaeec.cn/jx.php/?url="
+        },
+        {
+          value: "https://api.flvsp.com/?url=",
+          label: "https://api.flvsp.com/?url="
+        },
+        {
+          value: "http://2gty.com/apiurl/yun.php?url=",
+          label: "http://2gty.com/apiurl/yun.php?url="
+        },
+        {
+          value: "http://v.2gty.com/apiurl/yun.php?url=",
+          label: "http://v.2gty.com/apiurl/yun.php?url="
+        }
+      ]
+    };
+  },
+  methods: {
+    play() {
+      if (!this.parse_url || !this.play_url) {
+        this.$message.error("请选择解析地址，并输入视频地址！");
+        return;
+      }
+      if (!/^http.*$/.test(this.play_url)) {
+        this.$message.error("视频地址不正确！");
+        return;
+      }
+
+      window.open(this.parse_url + this.play_url)
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.box-card {
+  width: 80%;
+  margin: 10% auto;
+  height: 60vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.parse {
+  margin: 0px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.el-select-custom,
+.el-input-custom,
+.el-button-custom {
+  display: block;
+  width: 75%;
+  margin: auto;
+  margin-top: 30px;
 }
-a {
-  color: #42b983;
+
+hr {
+  border: none;
+  border-top: 1px solid #eee;
+  margin-bottom: 60px;
+  margin-top: 60px;
+  width: 90%;
 }
 </style>
